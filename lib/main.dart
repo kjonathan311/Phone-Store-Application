62a8e3cb3dce8a7ff0a795d7903e7eea1e4c2b63
erland-20 <79450864+erland-20@users.noreply.github.com>
2024-01-12 14:59:39 +0700

    add payment gateway flutter stripe

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_store_application/apikeys/apikeys.dart';
 import 'package:phone_store_application/cart_screen.dart';
 import 'package:phone_store_application/login_screen.dart';
 import 'package:phone_store_application/main_screen.dart';
 import 'package:phone_store_application/provider/cart_provider.dart';
 import 'package:phone_store_application/register_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 import 'firebase_options.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = APIkeys.publishableKey;
+  await Stripe.instance.applySettings();
   await Firebase.initializeApp(
          options: DefaultFirebaseOptions.currentPlatform,
   );
